2fd12e45879d20f7d7ad13585e7e4a3689efe7b7
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-02-11 14:35:33 +0100

    Exo6 last version

--- Semaine2/Exo6.java
+++ Semaine2/Exo6.java
@@ -1,16 +1,14 @@
 class Exo6 {
 	
 public static double scalarProduct(double v1[], double v2 []){
-	
 	return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
 	}
 
 
 public static double[] vectorialProduct(double v1[], double v2[]){
-double result [] = new double[3];
-	result[0] = v1[1]*v2[2] - v1[2]*v2[1];
-	result[1] = v1[2]*v2[0] - v1[0]*v2[2];
-	result[2] = v1[0]*v2[1] - v1[1]*v2[0];
+	double result [] = {v1[1]*v2[2] - v1[2]*v2[1], 
+						v1[2]*v2[0] - v1[0]*v2[2],
+						v1[0]*v2[1] - v1[1]*v2[0]};
 	return result;
 }
 public static double[] sumVector(double v1[], double v2[]){
@@ -20,6 +18,14 @@ public static double[] sumVector(double v1[], double v2[]){
 	return result;
 	}
 
+public static String asString(double v[]){
+	String result;
+	result = "{"+(new Double(v[0])).toString()+","
+				+(new Double(v[1])).toString()+","
+				+(new Double(v[2])).toString()
+				+"}";
+	return result;
+} 
 public static void testVecteur3D(){
 	double v1 [] = {1, 1, 1}; // v1[0] = 1; v1[1] = 1; v1[2] = 1;
 	double v2 [] = {2, 2, 2};
@@ -28,10 +34,8 @@ public static void testVecteur3D(){
 
 	System.out.println(scalarProduct(v1, v2) == 6);
 	System.out.println(scalarProduct(sumVector(v1, v2), v1) == 9);
-	double v3 [] = vectorialProduct(v1,zero);
-	System.out.println(v3[0] == 0);
-	System.out.println(v3[1] == 0);
-	System.out.println(v3[2] == 0);
+	System.out.println(asString(vectorialProduct(v1,zero)).
+			equals("{0.0,0.0,0.0}"));
 
 }
 
