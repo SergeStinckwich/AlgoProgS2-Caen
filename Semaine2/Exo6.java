618e03e6847221f9aaa8d29b1f73a7b74bbac05d
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-02-11 10:01:34 +0100

    Semaine2/Exo6

--- Semaine2/Exo6.java
+++ Semaine2/Exo6.java
@@ -5,6 +5,14 @@ public static double scalarProduct(double v1[], double v2 []){
 	return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
 	}
 
+
+public static double[] vectorialProduct(double v1[], double v2[]){
+double result [] = new double[3];
+	result[0] = v1[1]*v2[2] - v1[2]*v2[1];
+	result[1] = v1[2]*v2[0] - v1[0]*v2[2];
+	result[2] = v1[0]*v2[1] - v1[1]*v2[0];
+	return result;
+}
 public static double[] sumVector(double v1[], double v2[]){
 	double result [] = {v1[0]+v2[0],
 						v1[1]+v2[1],
