93ac6bddf9106332b99a3e7409549d15a2c70213
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-02-19 14:35:16 +0100

    Last version of Vector3D class

--- Semaine4/Vector3D.java
+++ Semaine4/Vector3D.java
@@ -7,37 +7,72 @@ private double z;
 public Vector3D(double xInit, double yInit, double zInit){
 	x = xInit; y = yInit; z = zInit;}
 
+public Vector3D(Vector3D v){
+	this.x = v.x;
+	this.y = v.y;
+	this.z = v.z;
+}
+
+public boolean equals(Vector3D v){
+	return (this.x == v.x)&&(this.y == v.y)&&(this.z == v.z);
+}
+
+public Vector3D copy(){
+	return new Vector3D(this.x, this.y, this.z);
+}
+
+public static double scalarProduct(Vector3D v1, Vector3D v2){
+		return v1.x*v2.x+v1.y*v2.y+v1.z*v2.z;
+}
+
 public double scalarProduct(Vector3D v){
-	return this.x*v.x+this.y*v.y+this.z*v.z;
+		return scalarProduct(this, v);
 	}
 
-public Vector3D vectorialProduct(Vector3D v){
-	return new Vector3D(this.y*v.z - this.z*v.y,
-					this.z*v.x - this.x*v.z,
-					this.x*v.y - this.y*v.x);
+public static Vector3D vectorialProduct(Vector3D v1, Vector3D v2){
+	return new Vector3D(v1.y*v2.z - v1.z*v2.y,
+					v1.z*v2.x - v1.x*v2.z,
+					v1.x*v2.y - v1.y*v2.x);
 }
-public Vector3D sumVector(Vector3D v){
-	return new Vector3D(this.x + v.x,
-						this.y + v.y,
-						this.z + v.z);
+
+public Vector3D vectorialProduct(Vector3D v){
+	return vectorialProduct(this, v);
+}
+
+public static Vector3D sum(Vector3D v1, Vector3D v2){
+	return new Vector3D(v1.x + v2.x,
+						v1.y + v2.y,
+						v1.z + v2.z);
+}
+public Vector3D sum(Vector3D v){
+	return sum(this, v);
 	}
 
 public String toString(){
-	return "Vector3D("+(new Double(x)).toString()+","
-				+(new Double(y)).toString()+","
-				+(new Double(z)).toString()
-				+")";
+	return "Vector3D("	+x+", "
+						+y+", "
+						+z
+					+")";
 }
 
 public static void main(String args[]){
-	Vector3D v1 	= new Vector3D(1, 1, 1);
-	Vector3D v2 	= new Vector3D(2, 2, 2);
-	Vector3D zero 	= new Vector3D(0, 0, 0);
-	Vector3D result = new Vector3D(3, 3, 3);
+	Vector3D v1 	= new Vector3D(1.0, 1.0, 1.0);
+	Vector3D v2 	= new Vector3D(2.0, 2.0, 2.0);
+	Vector3D v3  	= new Vector3D(v1);
+	Vector3D zero 	= new Vector3D(0.0, 0.0, 0.0);
+	Vector3D result = new Vector3D(3.0, 3.0, 3.0);
 
-	System.out.println(v1.scalarProduct(v2) == 6);
-	System.out.println((v1.sumVector(v2)).scalarProduct(v1) == 9);
-	System.out.println(v1.vectorialProduct(zero));
-	System.out.println(v1.vectorialProduct(zero).equals("Vector3D(0.0,0.0,0.0)"));
+	System.out.println(v1.scalarProduct(v2) == 6.0);
+	System.out.println(scalarProduct(v1,v2) == 6.0);
+
+	System.out.println((v1.sum(v2)).scalarProduct(v1) == 9.0);
+	System.out.println(scalarProduct(sum(v1, v2),v1) == 9.0);
+
+	System.out.println(v1.vectorialProduct(zero).toString().equals("Vector3D(0.0, 0.0, 0.0)"));
+	System.out.println(vectorialProduct(v1,zero).toString().equals("Vector3D(0.0, 0.0, 0.0)"));
+
+	System.out.println(v1.copy().equals(v1) == true);
+	System.out.println((v1==v3) == false);
+	System.out.println(v1.equals(v3) == true);
 	}
 }
