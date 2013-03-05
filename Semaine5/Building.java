9d7497815edca9d76e6c0654180fa85fbce77533
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-03-05 16:57:07 +0100

    Add Buildings&Factories

---
+++ Semaine5/Building.java
@@ -0,0 +1,34 @@
+class Building {
+	private String owner;
+	private double surface;
+	private String address;
+
+	public double computeTaxes(){return 5.0*surface;}
+
+	public String toString(){
+		return "Building("
+		+ "owner="
+		+ owner
+		+ ", surface="
+		+ surface
+		+ ", address="
+		+ address
+		+")";
+	}
+
+	public String getOwner(){return owner;}
+	public String getAddress(){return address;}
+	public double getSurface(){return surface;}
+
+	public Building(String owner, double surface, String address){
+		this.owner = owner;
+		this.surface = surface;
+		this.address = address;
+	}
+
+	public static void main(String[] args) {
+		Building myHouse = new Building("Me", 100, "My address");
+		System.out.println(myHouse);
+		System.out.println(myHouse.computeTaxes());
+	}
+}
---
+++ Semaine5/Factory.java
@@ -0,0 +1,28 @@
+class Factory extends Building {
+	String companyName;
+
+	public Factory(String owner, double surface,
+		String address, String companyName){
+		super(owner, surface, address);
+		this.companyName = companyName;
+	}
+
+	public String toString(){
+		return "Factory("
+		+ "owner="
+		+ this.getOwner()
+		+ ", surface="
+		+ this.getSurface()
+		+ ", address="
+		+ this.getAddress()
+		+ ", companyName="
+		+ this.companyName
+		+")";
+	}
+	
+	public static void main(String[] args){
+		Factory myFactory = new Factory("John", 1000, "San Francisco", "Google");
+		System.out.println(myFactory);
+		System.out.println(myFactory.computeTaxes());
+	}
+}
