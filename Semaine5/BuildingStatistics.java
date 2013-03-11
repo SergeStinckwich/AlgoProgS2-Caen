cd4421702f0de39aa79c2502b247a2e3d4068f29
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-03-11 11:58:06 +0100

    Add BuildingStatistics class

---
+++ Semaine5/BuildingStatistics.java
@@ -0,0 +1,23 @@
+class BuildingStatistics  {
+	
+	public static double meanTaxes(Building buildings[]){
+	int numberOfBuildings = buildings.length;
+	double sum = 0.0;
+	for (int i=0; i < numberOfBuildings-1; i++)
+		sum = sum + buildings[i].computeTaxes();
+	return sum/numberOfBuildings;
+}
+
+public static void main(String[] args) {
+		Building buildings[]= new Building[6];
+		buildings[0]= new Villa("John", 1000, "XXX", 100, true);
+		buildings[1]= new Villa("Mary", 2000, "YYY", 200, false);
+		buildings[2]= new Factory("Bill", 250, "XXXX", "AAA", 10000, 100);
+		buildings[3]= new Factory("Edgar", 500, "IIII", "BBB", 200, 10);
+		buildings[4]= new Building("John", 2000, "RRR");
+		buildings[5]= new Building("Bill", 3000, "ZZZ");
+
+		System.out.println(meanTaxes(buildings));
+	}
+
+}
