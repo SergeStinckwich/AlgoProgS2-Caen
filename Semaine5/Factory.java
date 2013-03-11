b26f2093acfd3960a8ab9b7230dca9e1f3cdee35
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-03-11 11:35:35 +0100

    Add Factory class

--- Semaine5/Factory.java
+++ Semaine5/Factory.java
@@ -1,10 +1,15 @@
 public class Factory extends Building {
 	String companyName;
+	int employeeNumber;
+	int deliveryNumber;
 
 	public Factory(String owner, double surface,
-		String address, String companyName){
+		String address, String companyName,
+		int employeeNumber, int deliveryNumber){
 		super(owner, surface, address);
 		this.companyName = companyName;
+		this.employeeNumber = employeeNumber;
+		this.deliveryNumber = deliveryNumber;
 	}
 
 	public String toString(){
@@ -17,11 +22,19 @@ public class Factory extends Building {
 		+ this.getAddress()
 		+ ", companyName="
 		+ this.companyName
+		+ ", employeeNumber="
+		+ this.employeeNumber
+		+ ", deliveryNumber="
+		+ this.deliveryNumber
 		+")";
 	}
 	
+	double vehiculeFlow(){
+		return 0.75*employeeNumber + deliveryNumber;
+	}
+
 	public static void main(String[] args){
-		Factory myFactory = new Factory("John", 1000, "San Francisco", "Google");
+		Factory myFactory = new Factory("John", 1000, "San Francisco", "Google", 10000, 100);
 		System.out.println(myFactory);
 		System.out.println(myFactory.computeTaxes());
 	}
