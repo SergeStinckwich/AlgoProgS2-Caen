84a141ccc1632d978090e64ed6765549245cf598
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-03-25 15:08:18 +0100

    New version

--- Semaine7/Liste.java
+++ Semaine7/Liste.java
@@ -14,20 +14,20 @@ public abstract class Liste {
 	public abstract void ajouterFin(Object element);
 	public abstract boolean estVide();
 	public abstract int longueur();
-	// public abstract boolean appartient(Object element);
-	// public abstract boolean noeudDansCetteListe(Noeud noeud);
+	public abstract boolean appartient(Object element);
+	public abstract boolean noeudDansCetteListe(Noeud noeud);
 	// public abstract Noeud ouEstElement(Object element);
 	// public abstract boolean supprimerPremier();
 	// public abstract boolean supprimerDernier();
 	public abstract Noeud iemeNoeud(int num);
-	// public Object iemeElement(int num){
-	// 	Noeud ieme =iemeNoeud(num);
-	// 	if (ieme==null) return null;
-	// 	return ieme.getElement();
-	// }
+	public Object iemeElement(int num){
+		Noeud ieme =iemeNoeud(num);
+		if (ieme==null) return null;
+		return ieme.getElement();
+		}
 	// public abstract int positionElement(Object element);
 	// public abstract int positionNoeud(Noeud noeud);
-	// public abstract Liste copieSuperficielle();
+	public abstract Liste copieSuperficielle();
 	// public abstract void insererApres(Noeud noeud, Object element);
 	// public abstract void insererAvant(Noeud noeud, Object element);
 	// public abstract void ajouterSimple(Liste l);
--- Semaine7/ListeSimple.java
+++ Semaine7/ListeSimple.java
@@ -16,11 +16,46 @@ public class ListeSimple extends Liste {
 		_premier = tmp.premierNoeud();
 		_dernier = tmp.dernierNoeud();
 	}
-	public boolean estVide()
-	{
-		return (_premier == null);
+
+	public Liste copieSuperficielle(){
+		Liste newListe = new ListeSimple();
+		NoeudSimple courant = (NoeudSimple)newListe.premierNoeud();
+		while(courant != null)
+		{
+			newListe.ajouterFin(courant.getElement());
+			courant = (NoeudSimple)courant.suivant();
+		}
+		return newListe;
+	}
+	public boolean estVide(){
+		return _premier == null;
 	}
 
+	public boolean appartient(Object element){
+		boolean elementTrouve = false;
+		NoeudSimple courant = _premier;
+		while(!elementTrouve)
+		{
+			if (courant == null) return false;
+			if ((courant.getElement()) == element)
+				elementTrouve = true;
+			else courant = (NoeudSimple)courant.suivant();
+		}
+		return true;
+	}
+
+	public boolean noeudDansCetteListe(Noeud n){
+		boolean noeudTrouve = false;
+		NoeudSimple courant = _premier;
+		while(!noeudTrouve){
+			if (courant == null) return false;
+			if (courant == n)
+				noeudTrouve = true;
+			else courant = (NoeudSimple)courant.suivant();
+		}
+		return true;
+	}
+	
 	public void ajouterDebut(Object element)
 	{
 		NoeudSimple n = new NoeudSimple(element);
@@ -87,20 +122,32 @@ public class ListeSimple extends Liste {
 	public static void main(String args[])
 	{
 		ListeSimple myList = new ListeSimple();
+		// Vérifier que la liste est vide
 		System.out.println(myList.estVide()==true);
+		// ajouter 5 en début de liste
 		myList.ajouterDebut(5);
+		// ajouter 6 en fin de liste
 		myList.ajouterFin(6);
+		// Vérifier que le premier noeud est 5
 		System.out.println(myList.premierNoeud().getElement().equals(5));
+		// Vérifier que la liste n'est plus vide
 		System.out.println(myList.estVide() == false);
+		// Vérifier que la liste est de longueur 2
 		System.out.println(myList.longueur() == 2);
+		// Vérifier que l'élément en position 1 est 5
 		System.out.println(myList.iemeNoeud(1).getElement().equals(5));
+		// Vérifier que l'élément en position 2 est 6
 		System.out.println(myList.iemeNoeud(2).getElement().equals(6));
+		// Vérifier qu'il y a pas d'élément en position 3
 		System.out.println(myList.iemeNoeud(3) == null);
 
 		int t[] = {1, 2, 3, 4, 5};
 		ListeSimple myList2 = new ListeSimple(t);
 		System.out.println(myList2.premierNoeud().getElement().equals(1));
 		System.out.println(myList2.dernierNoeud().getElement().equals(5));
-		
+		// 1 appartient à t
+		System.out.println(myList2.appartient(1) == true);
+		// 6 n'appartient pas à t
+		System.out.println(myList2.appartient(6) == false);
 	}
 }
