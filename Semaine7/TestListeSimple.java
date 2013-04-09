c53a6765402e52c5aa56561037857a5410791b5c
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-04-09 10:28:28 +0200

    Mimick a test framework

--- Semaine7/TestListeSimple.java
+++ Semaine7/TestListeSimple.java
@@ -2,6 +2,8 @@ public class TestListeSimple{
 
 	private static ListeSimple l1, l2;
 
+	private static int testCounter = 0;
+
 	public static void setUp()
 	{
 		l1 = new ListeSimple();
@@ -9,95 +11,94 @@ public class TestListeSimple{
 		l2 = new ListeSimple(t);
 	}
 
+	public static void assertTrue(String msg, boolean condition)
+	{
+		testCounter++;
+		System.out.print("Test " + testCounter + ":");
+		System.out.println(msg);
+		if (condition) System.out.println("Test OK");
+		else System.out.println("Test FALSE!!!!");
+	}
 	public static void main(String args[])
 	{
-		// Test1: vérifier qu'une nouvelle liste est vide
 		setUp();
-		System.out.println(l1.estVide() == true);
+		assertTrue("Vérifier qu'une nouvelle liste est vide", l1.estVide());
 
-		// Test2: Lorsqu'on ajoute un élément au début d'une liste vide
-		// la liste est de longueur 1
 		setUp();
 		l1.ajouterDebut(1);
-		System.out.println(l1.longueur() == 1);
+		assertTrue("Lorsqu'on ajoute un élément au début d'une liste vide, la liste est de longueur 1",
+			l1.longueur() == 1);
 
-		// Test3: Lorsqu'on ajoute un élément au début d'une liste avec un élément,
-		// la liste est de longueur 2
 		setUp();
 		l1.ajouterDebut(1);
 		l1.ajouterDebut(2);
-		System.out.println(l1.longueur() == 2 );
+		assertTrue("Lorsqu'on ajoute un élément au début d'une liste avec un élément, la liste est de longueur 2",
+			l1.longueur() == 2 );
 
-		// Test4: Lorsqu'on ajoute l'élément 1 en début de liste, le premier noeud est 1
 		setUp();
 		l1.ajouterDebut(1);
-		System.out.println(l1.premierNoeud().getElement().equals(1));
+		assertTrue("Lorsqu'on ajoute l'élément 1 en début de liste, le premier noeud est 1",
+			l1.premierNoeud().getElement().equals(1));
 
-		// Test5: Lorsqu'on ajoute un élément en fin d'une liste vide
-		// la liste est de longueur 1
 		setUp();
 		l1.ajouterFin(2);
-		System.out.println(l1.longueur() == 1);
+		assertTrue("Lorsqu'on ajoute un élément en fin d'une liste vide la liste est de longueur 1",
+			l1.longueur() == 1);
 
-		// Test6: Lorsqu'on ajoute un élément en fin d'une liste avec un élément,
-		// la liste est de longueur 2
 		setUp();
 		l1.ajouterFin(1);
 		l1.ajouterFin(2);
-		System.out.println(l1.longueur() == 2 );
+		assertTrue("Lorsqu'on ajoute un élément en fin d'une liste avec un élément, la liste est de longueur 2"
+			,l1.longueur() == 2 );
 
-		// Test7: Lorsqu'on ajoute l'élément 1 en fin d'une liste vide
-		// le dernier noeud est 1
 		setUp();
 		l1.ajouterFin(1);
-		System.out.println(l1.dernierNoeud().getElement().equals(1));
+		assertTrue("Lorsqu'on ajoute l'élément 1 en fin d'une liste vide le dernier noeud est 1",
+			l1.dernierNoeud().getElement().equals(1));
 
-		// Test8: Lorsqu'on ajoute l'élément 1 en fin d'une liste pas vide
-		// le dernier noeud est 1
 		setUp();
 		l1.ajouterFin(2);
 		l1.ajouterFin(1);
-		System.out.println(l1.dernierNoeud().getElement().equals(1));
+		assertTrue("Lorsqu'on ajoute l'élément 1 en fin d'une liste pas vide le dernier noeud est 1",
+			l1.dernierNoeud().getElement().equals(1));
 
-		// Test9: Une liste de longueur 2, n'a pas d'élément en position 2
 		setUp();
 		l1.ajouterFin(2);
 		l1.ajouterFin(1);
-		System.out.println(l1.iemeNoeud(2) == null);
+		assertTrue("Une liste de longueur 2, n'a pas d'élément en position 2",
+			l1.iemeNoeud(2) == null);
 
-		// Test10: Vérifier qu'un élément appartient à une liste
 		setUp();
-		System.out.println(l2.appartient(1) == true);
+		assertTrue("Vérifier qu'un élément appartient à une liste",
+			l2.appartient(1) == true);
 
-		// Test11: Vérifier qu'un élément n'appartient pas à une liste
 		setUp();
-		System.out.println(l2.appartient(11) == false);
+		assertTrue("Vérifier qu'un élément n'appartient pas à une liste",
+			l2.appartient(11) == false);
 
-		// Test12: Vérifier que le noeud avant le premier noeud n'existe pas
 		setUp();
-		System.out.println(l2.avant(l2.premierNoeud()) == null);
+		assertTrue("Vérifier que le noeud avant le premier noeud n'existe pas",
+			l2.avant(l2.premierNoeud()) == null);
 
-		// Test13: Vérifier que la liste l2 après suppression du premier noeud, commence par l'élement 2
 		setUp();
 		l2.supprimerPremier();
-		System.out.println(l2.premierNoeud().getElement().equals(2));
+		assertTrue("Vérifier que la liste l2 après suppression du premier noeud, commence par l'élement 2",
+			l2.premierNoeud().getElement().equals(2));
 
-		// Test14: Vérifier que la liste l2 après suppression du dernier noeud a
-		// comme dernier noeud 9.
 		setUp();
 		l2.supprimerDernier();
-		System.out.println(l2.dernierNoeud().getElement().equals(9));
+		assertTrue("Vérifier que la liste l2 après suppression du dernier noeud a comme dernier noeud 9",
+			l2.dernierNoeud().getElement().equals(9));
 
-		// Test15: Vérifier qu'insérer après le premier noeud, augmente la taille de la liste de 1
 		setUp();
 		l2.insererApres(l2.premierNoeud(), 1);
-		System.out.println(l2.longueur() == 11);
+		assertTrue("Vérifier qu'insérer après le premier noeud, augmente la taille de la liste de 1",
+			l2.longueur() == 11);
 
-		// Test16: Vérifier qu'après avoir inséré avant le premier noeud, l'élément 0, on a l'élément 0
-		//en tête de la liste
 		setUp(); 
 		l2.insererAvant(l2.premierNoeud(), 0);
-		System.out.println(l2.premierNoeud().getElement().equals(0));
+		assertTrue("Vérifier qu'après avoir inséré avant le premier noeud, l'élément 0, on a l'élément 0 en tête de la liste",
+			l2.premierNoeud().getElement().equals(0));
 
 	}
 }
